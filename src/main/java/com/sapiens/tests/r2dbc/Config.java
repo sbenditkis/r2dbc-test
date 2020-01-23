2cc43dc1de4229a9ab7c523bf9163d69b452619c
Sergey Benditkis <sergey.b@sapiens.com>
2020-01-23 13:21:32 +0200

    custom query impl - implemented by extension

--- src/main/java/com/sapiens/tests/r2dbc/Config.java
+++ src/main/java/com/sapiens/tests/r2dbc/Config.java
@@ -1,10 +1,10 @@
 package com.sapiens.tests.r2dbc;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 public class Config {
-
 }
